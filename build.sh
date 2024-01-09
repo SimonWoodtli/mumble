22df865efd6d8dd1e0dce4c3f9cdc03ac525f726
SimonWoodtli <xnasero@posteo.net>
2024-01-09 17:52:48 -0500

    🩹 add -y flag to confirm install

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ qt5-qmake qtbase5-dev-tools qttools5-dev qttools5-dev-tools \
 libqt5svg5-dev libboost-dev libssl-dev libprotobuf-dev protobuf-compiler \
 libprotoc-dev libcap-dev libxi-dev libasound2-dev libogg-dev \
 libsndfile1-dev libspeechd-dev libavahi-compat-libdnssd-dev \
-libxcb-xinerama0 libzeroc-ice-dev libpoco-dev g++-multilib
+libxcb-xinerama0 libzeroc-ice-dev libpoco-dev g++-multilib -y
 git clone https://github.com/mumble-voip/mumble.git /tmp/mumble
 cd /tmp/mumble
 git submodule update --init
