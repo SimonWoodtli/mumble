0f39d32f236dfa7dfc5ae870284011359a7be9ba
SimonWoodtli <xnasero@posteo.net>
2024-01-09 17:55:45 -0500

    🩹 add -y to apt update too

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 
 set -uex pipefail
 
-apt-get update
-apt-get install build-essential cmake pkg-config qtbase5-dev qtchooser \
+apt-get update -y
+apt-get install -y build-essential cmake pkg-config qtbase5-dev qtchooser \
 qt5-qmake qtbase5-dev-tools qttools5-dev qttools5-dev-tools \
 libqt5svg5-dev libboost-dev libssl-dev libprotobuf-dev protobuf-compiler \
 libprotoc-dev libcap-dev libxi-dev libasound2-dev libogg-dev \
 libsndfile1-dev libspeechd-dev libavahi-compat-libdnssd-dev \
-libxcb-xinerama0 libzeroc-ice-dev libpoco-dev g++-multilib -y
+libxcb-xinerama0 libzeroc-ice-dev libpoco-dev g++-multilib
 git clone https://github.com/mumble-voip/mumble.git /tmp/mumble
 cd /tmp/mumble
 git submodule update --init
